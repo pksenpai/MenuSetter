23f9e378d7e87ee7587adb5ef2147349842880c1
PKPY <p01001011p01011001@gmail.com>
2023-11-07 12:13:31 +0330

    debug & upgrade commander|add default menu.json file for use

--- menu_setter/commander.py
+++ menu_setter/commander.py
@@ -1,52 +1,163 @@
+"""\______________________________IMPORT MODULE______________________________/"""
 from setter import call_menu
-from ms_config import data_extractor
+from ms_config import data_extractor, extra_opt
 from pprint import pprint
+from json import dumps
+from shutil import copytree
+from os import path
 import argparse
 
+"""\______________________________DEFAULT MENU______________________________/"""
+def json_data():
+    default_menu={
+            "title": "Main Menu",
+            "act": None,
+            "sub": [
+                    {"title": "Login",
+                    "act": {
+                        "M": "test001",
+                        "F": "login",
+                        "args": [":D", 22],
+                        "C": "test",
+                        "attr": ["Parsa", "Ahmadian", "PKPY", "pk", "123"]
+                    },
+                    "sub": [
+                            {"title": "Edit Profile",
+                                "act": None,
+                                "sub": [
+                                        {"title": "Change Username",
+                                        "act": "change_username",  
+                                        "sub": None},
+                    
+                                        {"title": "Change Password",
+                                        "act": "change_password",
+                                        "sub": None}     
+                                    ]},
+
+                            {"title": "Contacts Panel",
+                                "act": None,
+                                "sub": [
+                                        {"title": "Add New Contact",
+                                        "act": "add_contact",
+                                        "sub": None}
+                                    ]}     
+                            ]},
+
+                    {"title": "SignUp",
+                    "act": {
+                        "M": "test001",
+                        "F": "signup",
+                        "C": "test",
+                        "attr": ["Parsa", "Ahmadian", "PKPY", "pk", "123"]
+                    },
+                    "sub": [
+                            {"title": "Show Name",
+                                "act": None,
+                                "sub": None}
+                            ]}
+                    ]
+            }
+
+    return default_menu
+
+
+"""\______________________________ARGPARSE______________________________/"""
 
 parser = argparse.ArgumentParser(
-                    prog='cms',
+                    prog='menu-commander',
                     description='commands for ms',
                     epilog='')
 
-"""\____________________________MAIN VARIABLE____________________________/"""
+"""\____________________________MAIN VARIABLE___________________________/"""
 
-parser.add_argument('command') # positional arg
+parser.add_argument(
+    'command',
+    choices=[
+        'ms-init', 
+        'ms-show', 
+        'ms-call'
+    ]
+)
+
 
 """\____________________________INIT OPTIONS____________________________/"""
 
-# optional that takes a value
-parser.add_argument('-n', '--name', 
-                    help='set a custom name for your menu header(default-name: >Main Menu<)'
-                ) 
+parser.add_argument(
+    '-n', '--name', 
+    help='set a custom name for your menu header(default-name: >Main Menu<)'
+) 
+parser.add_argument(
+    '-j', '--json',
+    action='store_true',
+    help='init a json file if ms-config directory exist'    
+)
 
-"""\____________________________CALL OPTIONS____________________________/"""
+"""\____________________________SHOW OPTIONS____________________________/"""
 
-parser.add_argument('-v', '--verbose', action='store_true', help='show more details') # on/off flag
+parser.add_argument('-v', '--verbose', action='store_true', help='show more menu details') # on/off flag
 
 #===========================================================
-args = parser.parse_args()
+args = parser.parse_args() # access to arguments           =
 #===========================================================
 
+"""\____________________________USEFUL FUNCS____________________________/"""
 
+def generate_json_file(PATH, FILE, EXT):
+    filePath = '{}/{}.{}'.format(PATH, FILE, EXT)
+    if not path.isfile(filePath):
+        menu = json_data()
+        if args.name: # give a custom name to menu name as menu header
+            new_name = args.name # new name
+            menu_name = new_name
+            menu["title"] = new_name
+                        
+        json_pf = dumps(menu, indent=4) # pretty formatter
+        if menu:
+            with open(filePath, 'w+') as json_file: # create file with site and doc link near other ms_config files
+                json_file.write(json_pf)
+        return f'{FILE}.{EXT} file added!:D', new_name
+    else:
+        return f'{FILE}.{EXT} already EXIST!:o', None
 
-"""\________________________________BODY_______________________________/"""
+def read_extra_opt():
+    PATH = extra_opt.menu_config_path
+    FILE = extra_opt.menu_config_fileName
+    EXT = extra_opt.menu_config_fileExtension
+    return PATH, FILE, EXT
+
+def read_json(): # read menu.json
+    return data_extractor.menu_ext()
+    
+"""\________________________________BODY________________________________/"""
 
 ###\____________FUNCS____________/###
-def init_menuSetter():
-    default_name = 'Main Menu'
-    menu_name = default_name
+def init_menuSetter(menu_name='Main Menu'): # default name ==> main menu    
+    PATH, FILE, EXT = read_extra_opt()
 
-    if args.name: # give a custom name to menu name as menu header
-        new_name = args.name
-        menu_name = new_name
-    
-    
-    return f'NICE: menu-setter files generated successfuly as >{menu_name}< :D'
+    if args.json:
+        msg, menu_name = generate_json_file(PATH, FILE, EXT)
+        print(msg, f'menu name: {menu_name}')
+        
+    elif not path.isdir(PATH):
+        """ Copy config files to parent_directory """
+        current_dir = path.abspath(path.dirname(__file__))
+        source_dir = path.join(current_dir, PATH)
+        new_configFile_name = PATH
+        
+        copytree(source_dir, new_configFile_name) # copying...
+        _, menu_name = generate_json_file(PATH, FILE, EXT)
+                
+        return f'NICE: menu-setter files generated successfuly as >{menu_name}< :D'
+        
+    else:
+        error=(f"{PATH} directory is already EXIST!:o" 
+               f"\nif you want to init a json file for menu input"
+               f"\nuse '-j' after 'ms-init' :3")
+        return error
     
 def show_menuSetter():
     # display menu with command
-    menu = data_extractor.menu_ext()
+    menu = read_json()
     oneline_menu = menu # %%%ONELINE_SHOW%%% in dev
     if args.verbose:
         # show more about menu options
@@ -56,21 +167,30 @@ def show_menuSetter():
     return oneline_menu
 
 def call_menuSetter():
-    # call menu with command
+    # move in menu with command
     for act in call_menu():
         if args.verbose:
             print(act)
         else:
             print(end="\r")
 
-    
-###\_____________CALL____________/###
-if args.command == "init-ms": # create config files <-- start with this command
-    print(init_menuSetter())
+try: 
+    ###\_____________CALL____________/###
+    if args.command == "ms-init": # create config files <-- start with this command
+        print(init_menuSetter())
 
-elif args.command == "show-ms":
-    pprint(show_menuSetter()) 
+    elif args.command == "ms-show":
+        pprint(show_menuSetter()) 
 
-# if open('connector.py', 'r'): # its mean menu has been inited with init-ms command
-elif args.command == "call-ms":
-    call_menuSetter()
+    # if open('connector.py', 'r'): # its mean menu has been inited with init-ms command
+    elif args.command == "ms-call":
+        call_menuSetter()
+
+    else:
+        print("this command is NOT EXIST!:(\n"
+              "use this command '[python/python3] menu-setter [-h][--help]' :)")
+
+except FileNotFoundError:
+    print("ms-config or ms-config/*.json is NOT EXIST!:("
+          "\nuse this command: '[python/python3] menu-setter ms-init'"
+          "\nfor create config files|")
